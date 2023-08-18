8286cfb31784eddc11160d906176630d09f9bb1f
imran qasim <imranqasim165@gmail.com>
2023-08-18 23:27:40 +0500

    firebase authentication in js

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCDL3U26Kvu16VxUcpsRtpL07dEIDwIgpQ",
+//     authDomain: "fir-project-a7458.firebaseapp.com",
+//     projectId: "fir-project-a7458",
+//     storageBucket: "fir-project-a7458.appspot.com",
+//     messagingSenderId: "575906290952",
+//     appId: "1:575906290952:web:0f2f7421abffc0db1420bd",
+//     measurementId: "G-6EF9ND3KRX"
+// };
+// Initialize Firebase
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBooUUH_SDZnQRz6l-J5Z9ZPsoi-zNrJM0",
+    authDomain: "auth-firebase-d875b.firebaseapp.com",
+    projectId: "auth-firebase-d875b",
+    storageBucket: "auth-firebase-d875b.appspot.com",
+    messagingSenderId: "499551448521",
+    appId: "1:499551448521:web:55cf160db5850688144c18",
+    measurementId: "G-60E8VH8XWY"
+  };
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
